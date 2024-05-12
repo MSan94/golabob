ef3b8a2f17d000a9d9a88cb76dfc2d3baefe9de1
MSan94 <ms1994s@naver.com>
2024-05-12 18:17:03 +0900

    viewmodel hilt 적용

--- app/src/main/java/com/ms/golabob/viewmodel/main/MainViewModel.kt
+++ app/src/main/java/com/ms/golabob/viewmodel/main/MainViewModel.kt
@@ -1,6 +1,9 @@
 package com.ms.golabob.viewmodel.main
 
 import com.ms.golabob.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainViewModel : BaseViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor() : BaseViewModel() {
 }
